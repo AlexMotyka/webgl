157729deb4f86dc5bc4513ae1bf059ab8f36be65
Alex Motyka <motykaalex@gmail.com>
2020-11-01 12:51:34 -0500

    Add colored square

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 4000
 
-app.use(express.static('public'))
+app.use(express.static('colored-square'))
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
