f505bfca3f00d3dc6d4f0a0fd549286da50e0341
Alex Motyka <motykaalex@gmail.com>
2020-11-02 09:26:40 -0500

    Add lighting cube

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 4000
 
-app.use(express.static('texture-cube'))
+app.use(express.static('lighting-cube'))
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
